1124a15d99d086617915b1eaa16f3e8928b581cd
development domain for misc project <misc@centos.code.version.server>
2013-03-01 15:18:01 +0800

    fixed left navigation

--- js/script.js
+++ js/script.js
@@ -11,9 +11,60 @@
 // - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
+    var RESPONSIVE_MODE_MINIMAL = 1;
+    var RESPONSIVE_MODE_MEDIUM = 2;
+    var RESPONSIVE_MODE_NORMALIZE = 3;
 
+    var navigation_toggle_by_button = false;
 
-// Place your code here.
+    // To determine the responsive mode
+    var judge_responsive_mode = function() {
+        if ($(window).width() > 960 ) { return RESPONSIVE_MODE_NORMALIZE;}
+        if ($(window).width() > 480 ) { return RESPONSIVE_MODE_MEDIUM;}
+        return RESPONSIVE_MODE_MEDIUM;
+    };
+
+    // locate sideber function
+    var sidebar_locate = function() {
+        var scroll_top = $(document).scrollTop();
+        $('.region-sidebar-first').animate({'top': scroll_top});
+
+        if (scroll_top > 0) {
+            $('#navigation-button').show();
+            // If navigation toggle by button, do nothing
+            if (navigation_toggle_by_button) {
+                return false;
+            }
+            $('#block-system-navigation').hide();
+        } else {
+            $('#block-system-navigation').show();
+            $('#navigation-button').hide();
+        }
+    };
+
+    var locate_timeout = 0;
+
+    // loaded observer
+    $(function() {
+        $(window).scroll(function() {
+            clearTimeout(locate_timeout);
+            if (judge_responsive_mode() >= RESPONSIVE_MODE_MEDIUM) {
+                locate_timeout = setTimeout(sidebar_locate, 500);
+            }
+        });
+
+        // navigation button click bind
+        $('#navigation-button').click(function() {
+            navigation_toggle_by_button = true;
+            $('#block-system-navigation').toggle();
+        });
+
+        // navigation more button click bind
+        $('#navigation-more-button').click(function() {
+            $('.region-navigation').show();
+            $('#navigation').animate({'height': '200px'});
+        });
+    });
 
 
 })(jQuery, Drupal, this, this.document);
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -72,49 +72,49 @@
 
 <div id="page">
 
-  <header id="header" role="banner">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
-    <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-
-  </header>
-
   <div id="main">
 
     <div id="content" class="column clearfix" role="main">
+      <header id="header" role="banner">
+
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php endif; ?>
+
+        <?php if ($site_name || $site_slogan): ?>
+          <hgroup id="name-and-slogan">
+            <?php if ($site_name): ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+
+            <?php if ($site_slogan): ?>
+              <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+            <?php endif; ?>
+          </hgroup><!-- /#name-and-slogan -->
+        <?php endif; ?>
+
+        <?php if ($secondary_menu): ?>
+          <nav id="secondary-menu" role="navigation">
+            <?php print theme('links__system_secondary_menu', array(
+              'links' => $secondary_menu,
+              'attributes' => array(
+                'class' => array('links', 'inline', 'clearfix'),
+              ),
+              'heading' => array(
+                'text' => $secondary_menu_heading,
+                'level' => 'h2',
+                'class' => array('element-invisible'),
+              ),
+            )); ?>
+          </nav>
+        <?php endif; ?>
+
+        <?php print render($page['header']); ?>
+      </header>
+
+
       <div class="content-left">
         <?php print render($page['highlighted']); ?>
         <?php print $breadcrumb; ?>
@@ -141,32 +141,7 @@
       <?php endif ?>
     </div><!-- /#content -->
 
-    <div id="navigation">
 
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div><!-- /#navigation -->
 
     <?php
       // Render the sidebars to see if there's anything in them.
@@ -183,8 +158,40 @@
 
   </div><!-- /#main -->
 
+
+
+
   <?php print render($page['footer']); ?>
 
 </div><!-- /#page -->
+
+<div id="navigation">
+
+  <?php if ($main_menu): ?>
+    <nav id="main-menu" role="navigation">
+      <?php
+      // This code snippet is hard to modify. We recommend turning off the
+      // "Main menu" on your sub-theme's settings form, deleting this PHP
+      // code block, and, instead, using the "Menu block" module.
+      // @see http://drupal.org/project/menu_block
+      print theme('links__system_main_menu', array(
+        'links' => $main_menu,
+        'attributes' => array(
+          'class' => array('links', 'inline', 'clearfix'),
+        ),
+        'heading' => array(
+          'text' => t('Main menu'),
+          'level' => 'h2',
+          'class' => array('element-invisible'),
+        ),
+      )); ?>
+    </nav>
+  <?php endif; ?>
+
+  <?php print render($page['navigation']); ?>
+  <div id="navigation-more-button">
+  </div>
+
+</div><!-- /#navigation -->
 
 <?php print render($page['bottom']); ?>
